4db04d24acb492723b941fd06d842cf7dfe73eeb
Filiphe <filiphe.vilar@outlook.com>
2017-08-30 15:35:32 -0300

    Models para Order

--- WingtipToys/WingtipToys/ShoppingCart.aspx.cs
+++ WingtipToys/WingtipToys/ShoppingCart.aspx.cs
@@ -1,14 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using WingtipToys.Models;
 using WingtipToys.Logic;
 using System.Collections.Specialized;
-using System.Collections;
-using System.Web.ModelBinding;
 
 namespace WingtipToys
 {
